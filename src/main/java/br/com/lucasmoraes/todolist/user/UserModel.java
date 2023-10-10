cefbb201d6b8b57476c6d62f15fda08bb54748e6
lucasearom <lucasmoraes211@gmail.com>
2023-10-10 16:39:49 -0300

    created user model file

--- src/main/java/br/com/lucasmoraes/todolist/user/UserModel.java
+++ src/main/java/br/com/lucasmoraes/todolist/user/UserModel.java
@@ -1,8 +1,31 @@
 package br.com.lucasmoraes.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
 
-    public String username;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID") // GERA UM VALOR ALEATÓRIO PARA O ID
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt; // DEFINE QUANDO O DADO FOI CRIADO
+
+    // MÉTODOS GETTERS E SETTERS PARA ATRIBUTOS PRIVADOS
+
 }
