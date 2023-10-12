b2fd322bb7cbbb799996a0ae6060821a1473328d
lucasearom <lucasmoraes211@gmail.com>
2023-10-12 11:48:13 -0300

    validando id de usuário para cadastro

--- src/main/java/br/com/lucasmoraes/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/lucasmoraes/todolist/filter/FilterTaskAuth.java
@@ -49,6 +49,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 // VALIDAR SENHA
                 var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                 if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
                     filterChain.doFilter(request, response);
                 } else {
                     response.sendError(401);
